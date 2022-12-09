6ed50b8ca1748012ddb9bde5a26deeca7b99c8a5
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2022-12-09 23:47:33 +0530

    Add: First name

--- src/main/java/com/regex/RegexEx.java
+++ src/main/java/com/regex/RegexEx.java
@@ -1,9 +1,22 @@
 package com.regex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexEx {
 
 	public static void main(String[] args) {
-		System.out.println( "Regex Practice Problem" );
+		Scanner sc = new Scanner(System.in);
+		Pattern pat1 = Pattern.compile("^[A-Z][a-z]{2,}");
+		System.out.println("Enter first name starts with Cap and has minimum 3 characters : ");
+		String firstname = sc.next();
+		Matcher mf = pat1.matcher(firstname);
+
+		if (mf.matches())
+			System.out.println("You have entered valid first name!");
+		else
+			System.out.println("You have entered invalid first name!");
 	}
 
 }
