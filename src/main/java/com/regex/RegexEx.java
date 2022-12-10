291d32513f62b4fa8af9ec64af3c246d52371685
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2022-12-10 19:08:32 +0530

    Add: Validate mobile number of User

--- src/main/java/com/regex/RegexEx.java
+++ src/main/java/com/regex/RegexEx.java
@@ -17,26 +17,36 @@ public class RegexEx {
 			System.out.println("You have entered valid first name!");
 		else
 			System.out.println("You have entered invalid first name!");
-		
+
 		System.out.println("Enter last name starts with Cap and has minimum 3 characters : ");
 		String lastname = sc.next();
-		Pattern pt2 = Pattern.compile("^[A-Z][a-z]{2,}"); 
+		Pattern pt2 = Pattern.compile("^[A-Z][a-z]{2,}");
 		Matcher ml = pat1.matcher(firstname);
 
 		if (ml.matches())
 			System.out.println("You have entered valid last name!");
 		else
 			System.out.println("You have entered invalid last name!");
-		
+
 		System.out.println("Enter your email");
 		String email = sc.next();
-		Pattern pt3 = Pattern.compile("^[a-z]{3,}([.][a-z]{3,})?[@][a-z]{2,}[.]co([.]in)?$"); 
+		Pattern pt3 = Pattern.compile("^[a-z]{3,}([.][a-z]{3,})?[@][a-z]{2,}[.]co([.]in)?$");
 		Matcher mail = pt3.matcher(email);
-		
-		if(mail.matches())
+
+		if (mail.matches())
 			System.out.println("You have entered Valid email!");
 		else
 			System.out.println("You have entered Invalid email!");
+
+		System.out.println("Enter your Mobile Number: ");
+        String mobileNumber = sc.next();
+        String pt4="^(91)[ ]{1}[1-9]{1}[0-9]{9}$";
+        boolean mn = Pattern.matches(pt4, mobileNumber);
+        if (mn) {
+            System.out.println("You have entered valid Mobile Number");
+        } else {
+            System.out.println("You have entered invalid Mobile Number");
+        }
 	}
 
 }
