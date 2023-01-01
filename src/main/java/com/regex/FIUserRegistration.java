ad24ec7ab4d1899a09c3925aac7899065f061b15
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2023-01-01 17:48:09 +0530

    Refactored: codes for lambda expression and functional interface

---
+++ src/main/java/com/regex/FIUserRegistration.java
@@ -0,0 +1,6 @@
+package com.regex;
+
+@FunctionalInterface
+public interface FIUserRegistration {
+	abstract boolean checkValidation(String s1 , String s2);
+}
--- src/main/java/com/regex/UserRegistrationForm.java
+++ src/main/java/com/regex/UserRegistrationForm.java
@@ -2,40 +2,48 @@ package com.regex;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-
+/**
+ * (UC-13_Lambda)
+ Refactor the Code to use Lambda Function to validate User Entry
+ - Use Lambda Function to validate First_Name, Last_Name, Email, Mobile, and Password */
 public class UserRegistrationForm {
 
-	static Scanner sc=new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-    	
+        // Master
         System.out.println("Welcome to User Registration program");
-        UserRegistrationForm urf = new UserRegistrationForm();
+
         System.out.println("Please enter your First Name: ");
         String firstName = sc.nextLine();
-        urf.checkFirstName(firstName);
+        checkFirstName(firstName);
 
         System.out.println("Please enter your Last Name: ");
         String lastName = sc.nextLine();
-        urf.checkLastName(lastName);
+        checkLastName(lastName);
 
         System.out.println("Please enter your Email Address: ");
         String email = sc.nextLine();
-        urf.checkEmailAddress(email);
+        checkEmailAddress(email);
 
         System.out.println("Please enter your Mobile Number: ");
         String mobileNumber = sc.nextLine();
-        urf.checkMobileNumber(mobileNumber);
+        checkMobileNumber(mobileNumber);
 
         System.out.println("Please enter your Password: ");
         String password = sc.nextLine();
-        urf.checkPassword(password);
+        checkPassword(password);
         System.out.println("Thank you for using Registering user!");
         sc.close();
     }
 
-    public boolean checkPassword(String password) {
+    public static boolean checkPassword(String password) {
+       
         String passwordPattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[~!@#$%^&*()<>:{},.;'])([A-Za-z0-9~!@#$%^&*()<>:{},.;']){8,}$";
-        boolean passwordCheck = Pattern.matches(passwordPattern, password);
+        //boolean passwordCheck = Pattern.matches(passwordPattern, password);
+        FIUserRegistration patternMatcher = (i1, i2) -> Pattern.matches(i2, i1);
+        boolean passwordCheck = patternMatcher.checkValidation(passwordPattern, password);
+
         int count = 0;
         for (int i = 0; i < password.length(); i++) {
             if (!Character.isDigit(password.charAt(i)) && !Character.isLetter(password.charAt(i))
@@ -65,10 +73,12 @@ public class UserRegistrationForm {
 
     }
 
-    public boolean checkMobileNumber(String mobileNumber) {
+    public static boolean checkMobileNumber(String mobileNumber) {
         // UC4: Mobile Number
-        String mobileNumberPattern = "^[+91]{3,}[ ][0-9]{10}$";
-        boolean mnCheck = Pattern.matches(mobileNumberPattern, mobileNumber);
+        String mobileNumberPattern = "^[+91]{3,} [0-9]{10}$";
+        FIUserRegistration patternMatcher = (i1, i2) -> Pattern.matches(mobileNumberPattern, mobileNumber);
+        boolean mnCheck = patternMatcher.checkValidation(mobileNumberPattern, mobileNumber);
+        //boolean mnCheck = Pattern.matches(mobileNumberPattern, mobileNumber);
         if (mnCheck) {
             System.out.println("You have entered valid Mobile Number");
         } else {
@@ -82,15 +92,12 @@ public class UserRegistrationForm {
         return mnCheck;
     }
 
-    public boolean checkEmailAddress(String email) {
-        // UC3: Email
+    public static boolean checkEmailAddress(String email) {
+        
+        //boolean emailCheck = Pattern.matches(emailPattern, email);
         String emailPattern = "^[a-z0-9]{3,}[+.-]?[a-z0-9]{0,}[@]{1,}[a-z0-9]{1,}[.]{1,}([a-z]{0,}[.]{0,})[a-z]{2,}$";
-        /*
-         * UC9:Email Sample to check abc@yahoo.com abc-100@yahoo.com abc.100@yahoo.com
-         * abc111@abc.com abc-100@abc.net abc.100@abc.com.au abc@1.com abc@gmail.co
-         * abc+100@gmail.com
-         */
-        boolean emailCheck = Pattern.matches(emailPattern, email);
+        FIUserRegistration patternMatcher = (i1, i2) -> Pattern.matches(emailPattern, email);
+        boolean emailCheck = patternMatcher.checkValidation(emailPattern, email);
         if (emailCheck) {
             System.out.println("You have entered valid Email Address");
         } else {
@@ -105,9 +112,12 @@ public class UserRegistrationForm {
 
     }
 
-    public boolean checkLastName(String lastName) {
+    public static boolean checkLastName(String lastName) {
         // UC2:Last Name
-        boolean lastNameCheck = Pattern.matches("^[A-Z][a-z]{2,}$", lastName);
+        //boolean lastNameCheck = Pattern.matches("^[A-Z][a-z]{2,}$", lastName);
+        String lastNamePattern = "^[A-Z][a-z]{2,}$";
+        FIUserRegistration patternMatcher = Pattern::matches;
+        boolean lastNameCheck = patternMatcher.checkValidation(lastNamePattern, lastName);
         if (lastNameCheck) {
             System.out.println("You have entered valid Last Name");
         } else {
@@ -124,8 +134,11 @@ public class UserRegistrationForm {
 
     public static boolean checkFirstName(String firstName) {
         // UC1:First Name
-        boolean res = Pattern.matches("^[A-Z][a-z]{2,}$", firstName);
-        if (res) {
+        //boolean res = Pattern.matches("^[A-Z][a-z]{2,}$", firstName);
+        String firstNamePattern = "^[A-Z][a-z]{2,}$";
+        FIUserRegistration patternMatcher = (p1, p2) -> Pattern.matches(p1, p2);
+        boolean firstNameCheck = patternMatcher.checkValidation(firstNamePattern, firstName);
+        if (firstNameCheck) {
             System.out.println("You have entered valid First Name");
         } else {
             try {
@@ -135,6 +148,6 @@ public class UserRegistrationForm {
                 System.out.println("Wrong Test");
             }
         }
-        return res;
+        return firstNameCheck;
     }
 }
